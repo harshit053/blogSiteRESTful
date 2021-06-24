2ac54eab07aef6063e47ef1e1c3b37059a27cf09
harshit053 <harshitgupta.gh53@gmail.com>
2021-06-24 18:11:07 +0530

    Hosting

--- app.js
+++ app.js
@@ -109,6 +109,6 @@ app.delete("/blogs/:id", function(req, res){
     });
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog Server Is Up!!!");
 });
